a1e113be3b38a737adfb53e1ab4fcd096d7f3b2b
Philipp Hancke <fippo@andyet.net>
2014-06-11 21:28:12 +0200

    we can update the hash later, thanks lance

--- filetransfer.js
+++ filetransfer.js
@@ -2,11 +2,7 @@ var async = require('async');
 var webrtcsupport = require('webrtcsupport');
 var WildEmitter = require('wildemitter');
 var util = require('util');
-//var crypto = require('crypto');
-
-// if (!(window.File && window.FileReader && window.FileList && window.blob)) 
-//    notsupported
-console.log(webrtcsupport);
+var crypto = require('crypto');
 
 function Sender() {
     WildEmitter.call(this);
@@ -15,6 +11,8 @@ function Sender() {
     this.pacing = 50;
     this.file = null;
     this.channel = null;
+
+    this.sha = crypto.createHash('sha1');
 
     // paced sender
     // TODO: do we have to do this?
@@ -25,6 +23,11 @@ function Sender() {
                 self.emit('progress', task.start, task.file.size);
                 self.channel.send(e.target.result);
                 window.setTimeout(next, self.pacing); // pacing
+
+                // as lance points out, xep-0234 allows us to update the hash
+                // later, so we can calculate it based on the chunks
+                //self.sha.update(e.target.result);
+                //console.log('hash', sha.digest('hex'));
             };
         })(task.file);
         var slice = task.file.slice(task.start, task.start + task.chunksize);
@@ -35,20 +38,6 @@ util.inherits(Sender, WildEmitter);
 
 Sender.prototype.send = function (file, channel) {
     this.file = file;
-
-    /*
-    // calculate the sha1 hash
-    // bad because it reads the whole file at once
-    var reader = new window.FileReader();
-    reader.onload = (function() {
-        return function(e) {
-            var sha = crypto.createHash('sha1');
-            sha.update(e.target.result);
-            console.log('hash', sha.digest('hex'));
-        };
-    })(file);
-    reader.readAsBinaryString(file);
-    */
 
     this.channel = channel;
     // FIXME: hook to channel.onopen?
@@ -78,13 +67,18 @@ Receiver.prototype.receive = function (metadata, channel) {
     this.channel = channel;
     this.channel.onmessage = function (event) {
         // weird
-        self.received += webrtcsupport.prefix === 'moz' ? event.data.size : event.data.byteLength;
-        // FIXME: what if > filesize?
+        var len = webrtcsupport.prefix === 'moz' ? event.data.size : event.data.byteLength;
+        self.received += len;
         self.receiveBuffer.push(event.data);
         self.emit('progress', self.received, self.metadata.size);
         if (self.received == self.metadata.size) {
             self.emit('receivedFile', new window.Blob(self.receiveBuffer), self.metadata);
             // FIXME: discard? close channel?
+        } else if (self.received > self.metadata.size) {
+            // FIXME
+            console.error('received more than expected, discarding...');
+            self.receiveBuffer = []; // just discard...
+
         }
     };
 };
